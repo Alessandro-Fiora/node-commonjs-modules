069d98844369afc29a5dd744b8e339400d05660a
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-11-18 14:53:58 +0100

    fullname using absolute path

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-const fullName = require("./utils/names");
+const path = require("path");
+const fullNamePath = path.join(__dirname, "utils", "names");
+const fullName = require(fullNamePath);
 
 console.log(fullName("Alessandro", "Fiora").surname);
